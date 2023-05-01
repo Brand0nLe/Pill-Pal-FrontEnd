6c3f42fb2df62af18abe7e87e8f76ab606a8f8ff
Your Name <wilkinsnigel2000@gmail.com>
2023-04-30 21:25:35 -0700

    prepping to push

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import LoginPage from '../src/components/pages/loginpage/LoginPage';
 import DashboardPage from './components/pages/dashboardpage/DashboardPage';
+import DependentsPage from './components/pages/dependentspage/DependentsPage';
 
 
 function App() {
   return (
     <div className="App">
-      < DashboardPage />
+      < DependentsPage />
 
     </div>
   );
--- src/components/pages/dependentspage/DependentsPage.tsx
+++ src/components/pages/dependentspage/DependentsPage.tsx
@@ -1,1 +1,14 @@
-<></>
+
+
+import React from 'react'
+import './DependentsPage.css'
+
+export default function DependentsPage() {
+  return (
+    <div className='parent-bg'>
+        
+    </div>
+  )
+}
+
+
