4fccc339359cc5cffb43fee2f77d1be17776c30c
Brand0nLe <93brandonle@gmail.com>
2023-05-02 00:16:37 -0700

    Testing Calendar.

--- src/components/pages/schedulepage/SchedulePage.tsx
+++ src/components/pages/schedulepage/SchedulePage.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Container, Row, Col, Form } from 'react-bootstrap';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Calendar from './calendar/Calendar';
+import dayjs from 'dayjs';
 
-const theme = createTheme();
+const SchedulePage: React.FC = () => {
+  const [selectedDate, setSelectedDate] = React.useState<dayjs.Dayjs>(dayjs());
 
-const SchedulePage: any = () => {
-return(
+  const handleDateChange = (date: dayjs.Dayjs | null) => {
+    if (date) {
+      setSelectedDate(date);
+    }
+  };
+
+  return (
     <Container>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-<h1>TESTING TEXT Schedule PAGE</h1>
-        </Container>
-);
-    };
+      <h1 className="text-center">Schedule Page</h1>
+      <Row className="justify-content-center">
+        <Col md={4}>
+          <Calendar selectedDate={selectedDate} handleDateChange={handleDateChange} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default SchedulePage;
+
 
--- src/components/pages/schedulepage/calendar/Calendar.tsx
+++ src/components/pages/schedulepage/calendar/Calendar.tsx
@@ -1,1 +1,28 @@
-<></>
+import * as React from 'react';
+import { LocalizationProvider, StaticDatePicker } from '@mui/lab';
+import DayjsAdapter from '@mui/lab/AdapterDayjs';
+import { TextField, TextFieldProps } from '@mui/material';
+import dayjs from 'dayjs';
+
+interface CalendarProps {
+  selectedDate: dayjs.Dayjs;
+  handleDateChange: (date: dayjs.Dayjs | null) => void;
+}
+
+const Calendar: React.FC<CalendarProps> = ({ selectedDate, handleDateChange }) => {
+  return (
+    <LocalizationProvider dateAdapter={DayjsAdapter}>
+      <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+        label="Select a date"
+        value={selectedDate}
+        onChange={(newValue: dayjs.Dayjs | null) => {
+          handleDateChange(newValue);
+        }}
+        renderInput={(params: TextFieldProps) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default Calendar;
