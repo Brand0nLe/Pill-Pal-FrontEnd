796c396096ca7f9e63551766f751bd470ff6c291
Brand0nLe <93brandonle@gmail.com>
2023-05-13 11:39:27 -0700

    bootstrap import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer';
 import AppRoutes from './components/services/RouterService';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const App = () => {
