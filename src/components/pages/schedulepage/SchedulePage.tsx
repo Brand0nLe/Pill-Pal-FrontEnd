49c841da4cd0824e42ce5ab2855463bdf6ea67e2
Brand0nLe <93brandonle@gmail.com>
2023-05-14 01:16:24 -0700

    got desktop calendar to appear

--- src/components/pages/schedulepage/SchedulePage.tsx
+++ src/components/pages/schedulepage/SchedulePage.tsx
@@ -1,47 +1,40 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FirstComponent from './calendar/Calendar';
-import dayjs from 'dayjs';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import NavBar from '../../navbarheader/NavBarHeader';
 
-
-
 const SchedulePage: React.FC = () => {
-  const [selectedDate, setSelectedDate] = React.useState<dayjs.Dayjs>(dayjs());
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const handleDateChange = (date: dayjs.Dayjs | null) => {
-    if (date) {
-      setSelectedDate(date);
-    }
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
   };
 
   return (
-
     <Container>
-      <NavBar/>
-          <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <h1 className="text-center">Schedule Page</h1>
       <Row className="justify-content-center">
         <Col md={4}>
-        <FirstComponent/>
-                </Col>
+          <Calendar value={selectedDate} onChange={handleDateChange} />
+        </Col>
       </Row>
-      <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </Container>
   );
 };
 
 export default SchedulePage;
-
-
---
+++ src/react-datepicker.d.ts
@@ -0,0 +1,2 @@
+declare module 'react-datepicker';
+declare module 'react-calendar';
