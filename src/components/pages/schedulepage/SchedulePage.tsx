62fc1c5b3168521fb2ee59a7becd8759248c7284
Your Name <wilkinsnigel2000@gmail.com>
2023-05-14 13:07:02 -0700

    errors to resolve, page is still functional

--- src/components/pages/schedulepage/SchedulePage.tsx
+++ src/components/pages/schedulepage/SchedulePage.tsx
@@ -1,64 +1,92 @@
 
 
-import React, {useState} from 'react';
-
-import DisabledByDefaultOutlinedIcon from '@mui/icons-material/DisabledByDefaultOutlined';
-import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
-import './SchedulePage.css'
-
+import React, { useState } from 'react';
+import { DisabledByDefaultOutlined, CheckBoxOutlined } from '@mui/icons-material';
+import './SchedulePage.css';
 
 export default function SchedulePage() {
-  // save our current date and day of the week
   const currentDate = new Date();
   const currentDayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
 
-  // here lets create our useState for the form to add meds
+  const [showForm, setShowForm] = useState(false); // State for showing the medication form
 
-  // delete this drug from the current display console log it
+  const [medications, setMedications] = useState([
+    { id: 1, time: '8:00 AM', name: 'Medication 1', dose: '10mg', instructions: 'Take with water' },
+    { id: 2, time: '10:30 AM', name: 'Medication 2', dose: '20mg', instructions: 'Take with food' },
+  ]);
 
   const handleDisableClick = (id: number) => {
     setMedications(prevMedications => prevMedications.filter(medication => medication.id !== id));
   };
-  const [medications, setMedications] = useState([
-    { id: 1, time: '8:00 AM', name: 'Medication 1', dose: '10mg', instructions: 'Take with water' },
-    { id: 2, time: '10:30 AM', name: 'Medication 2', dose: '20mg', instructions: 'Take with food' },
+
+  const handleAddMedication = () => {
+    setShowForm(true);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     
-  ]);
+    const time = (event.target as HTMLFormElement).elements.time.value;
+    const name = (event.target as HTMLFormElement).elements.name.value;
+    const dose = (event.target as HTMLFormElement).elements.dose.value;
+    const instructions = (event.target as HTMLFormElement).elements.instructions.value;
+
+
+    const newMedication = {
+      id: Date.now(), 
+      time,
+      name,
+      dose,
+      instructions,
+    };
+
+    setMedications(prevMedications => [...prevMedications, newMedication]);
+
+    // Reset the form and hide it
+    (event.target as HTMLFormElement).reset();
+    setShowForm(false);
+  };
 
   return (
     <>
-    <button>Add Medication</button>
-    <div className='table-parent'>
-      <div>
-
-        <table>
-          <thead>
-            <th>{currentDayOfWeek} {currentDate.toLocaleDateString()}</th>
-
-          </thead>
-          <tbody>
+      <button onClick={handleAddMedication}>Add Medication</button>
+      {showForm && (
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" name="time" placeholder="Time" required />
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="text" name="dose" placeholder="Dose" required />
+          <input type="text" name="instructions" placeholder="Instructions" required />
+          <button type="submit">Add</button>
+        </form>
+      )}
+      <div className="table-parent">
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>{currentDayOfWeek} {currentDate.toLocaleDateString()}</th>
+              </tr>
+            </thead>
+            <tbody>
               {medications.map(medication => (
                 <tr key={medication.id}>
-                  <td id='time'>{medication.time}</td>
+                  <td>{medication.time}</td>
                   <td>{medication.name}</td>
                   <td>{medication.dose}</td>
                   <td>{medication.instructions}</td>
                   <td>
-                    <DisabledByDefaultOutlinedIcon onClick={() => handleDisableClick(medication.id)} />
+                    <DisabledByDefaultOutlined onClick={() => handleDisableClick(medication.id)} />
                   </td>
                   <td>
-                    <CheckBoxOutlinedIcon />
+                    <CheckBoxOutlined />
                   </td>
                 </tr>
               ))}
             </tbody>
-        </table>
-      
-
+          </table>
+        </div>
       </div>
-
-    </div>
-    
     </>
-  )
+  );
 }
+
