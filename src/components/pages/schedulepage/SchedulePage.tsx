bce5db6491f6fc9fa120f8cf8d402b86b4f4dcbd
Brand0nLe <93brandonle@gmail.com>
2023-05-15 04:54:14 -0700

    modal opens but need data to store

--- src/components/pages/schedulepage/SchedulePage.tsx
+++ src/components/pages/schedulepage/SchedulePage.tsx
@@ -1,11 +1,13 @@
+import "./calendar/Calendar.css";
+import "../schedulepage/SchedulePage.css";
 import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './calendar/Calendar.css';
+import '../schedulepage/SchedulePage.css';
 import NavBar from '../../navbarheader/NavBarHeader';
-import { Button } from 'react-bootstrap';
-
+import { Button, Modal } from 'react-bootstrap';
 
 const SchedulePage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -38,101 +40,80 @@ const SchedulePage = () => {
 
   return (
     <div className="wrapper-parent">
-      <NavBar/>
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="col-lg-6">
-          <div className="calendar-container">
-            <Calendar value={selectedDate} onChange={handleDateChange} />
-            <Button
-              className="btn mt-3"
-              onClick={() => setShowModal(true)}
-            >
-              Add Medication
-            </Button>
+      <NavBar />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="calendar-container">
+              <Calendar value={selectedDate} onChange={handleDateChange} />
+              <Button
+                className="btn mt-3"
+                onClick={() => setShowModal(true)}
+              >
+                Add Medication
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="col-lg-6">
-          <div className="medication-list-container">
-            <h2>Medications for {selectedDate.toLocaleDateString()}</h2>
-            <ul>
-              {medications.map((medication, index) => (
-                <li key={index}>
-                  <span>{medication.name}</span>
-                  <span>{medication.time}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-      {showModal && (
-        <div className="modal" tabIndex={-1} role="dialog">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Add Medication</h5>
-                <Button
-                  type="button"
-                  className="close"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span>&times;</span>
-                </Button>
-              </div>
-              <div className="modal-body">
-                <div className="form-group">
-                  <label htmlFor="medName">Medication Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="medName"
-                    value={newMedication.name}
-                    onChange={(e) =>
-                      setNewMedication({
-                        ...newMedication,
-                        name: e.target.value,
-                      })
-                    }
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="medTime">Medication Time</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="medTime"
-                    value={newMedication.time}
-                    onChange={(e) =>
-                      setNewMedication({
-                        ...newMedication,
-                        time: e.target.value,
-                      })
-                    }
-                  />
-                </div>
-                </div>
-              <div className="modal-footer">
-                <Button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={() => setShowModal(false)}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={handleAddMedication}
-                >
-                  Add
-                </Button>
-              </div>
+          <div className="col-lg-6">
+            <div className="medication-list-container">
+              <h2>Medications for {selectedDate.toLocaleDateString()}</h2>
+              <ul>
+                {medications.map((medication, index) => (
+                  <li key={index}>
+                    <span>{medication.name}</span>
+                    <span>{medication.time}</span>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Medication</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="form-group">
+            <label htmlFor="medName">Medication Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="medName"
+              value={newMedication.name}
+              onChange={(e) =>
+                setNewMedication({
+                  ...newMedication,
+                  name: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="medTime">Medication Time</label>
+            <input
+              type="text"
+              className="form-control"
+              id="medTime"
+              value={newMedication.time}
+              onChange={(e) =>
+                setNewMedication({
+                  ...newMedication,
+                  time: e.target.value,
+                })
+              }
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleAddMedication}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
