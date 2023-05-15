7fc1f1f7bfc79b7790461069e4a8f754e38776f6
Brand0nLe <93brandonle@gmail.com>
2023-05-15 02:56:57 -0700

    footer doesn't have weird positioning anymore

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-      <footer style={{ position: 'relative', bottom: '0', width: '100%' }}>
-        <Container fluid className="py-3 d-flex justify-content-between align-items-center bg-dark text-white">
-          <p className="m-0">© Pill Pal 2023. All rights reserved.</p>
-          <div>
-            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-white me-3">
-              <LinkedInIcon fontSize="large" />
-            </a>
-            <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="text-white">
-              <GitHubIcon fontSize="large" />
-            </a>
-          </div>
-        </Container>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer style={{ position: 'fixed', bottom: '0', width: '100%' }}>
+      <Container fluid className="py-3 d-flex justify-content-between align-items-center bg-dark text-white">
+        <p className="m-0">© Pill Pal 2023. All rights reserved.</p>
+        <div>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-white me-3">
+            <LinkedInIcon fontSize="large" />
+          </a>
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="text-white">
+            <GitHubIcon fontSize="large" />
+          </a>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
