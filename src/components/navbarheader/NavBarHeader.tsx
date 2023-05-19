bb1540beb33ef4fd5878636af42d08c609aa8b29
Brand0nLe <93brandonle@gmail.com>
2023-05-19 14:01:18 -0700

    added active state for nav bar

--- src/components/navbarheader/NavBarHeader.tsx
+++ src/components/navbarheader/NavBarHeader.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../navbarheader/NavBarHeader.css';
 
 const NavBar: React.FC = () => {
+  const location = useLocation();
+  const [activePage, setActivePage] = useState(location.pathname);
+
   const handleLogout = () => {
     window.location.href = '/login';
+  };
+
+  const handleNavItemClick = (path: string) => {
+    setActivePage(path);
   };
 
   return (
@@ -18,22 +25,42 @@ const NavBar: React.FC = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto NavFont">
             <Nav.Item>
-              <Nav.Link as={Link} to="/dependentspage">
+              <Nav.Link
+                as={Link}
+                to="/dependentspage"
+                onClick={() => handleNavItemClick('/dependentspage')}
+                className={activePage === '/dependentspage' ? 'active' : ''}
+              >
                 Profiles
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/dashboardpage">
+              <Nav.Link
+                as={Link}
+                to="/dashboardpage"
+                onClick={() => handleNavItemClick('/dashboardpage')}
+                className={activePage === '/dashboardpage' ? 'active' : ''}
+              >
                 Dashboard
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/schedulepage">
+              <Nav.Link
+                as={Link}
+                to="/schedulepage"
+                onClick={() => handleNavItemClick('/schedulepage')}
+                className={activePage === '/schedulepage' ? 'active' : ''}
+              >
                 Schedule
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link as={Link} to="/historypage">
+              <Nav.Link
+                as={Link}
+                to="/historypage"
+                onClick={() => handleNavItemClick('/historypage')}
+                className={activePage === '/historypage' ? 'active' : ''}
+              >
                 History
               </Nav.Link>
             </Nav.Item>
