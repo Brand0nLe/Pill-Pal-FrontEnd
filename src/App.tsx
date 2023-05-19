98fe92f3c898c1fa4ff69ed231435ec160815d64
Brand0nLe <93brandonle@gmail.com>
2023-05-19 11:27:04 -0700

    small fixes and getting rid of errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './components/services/RouterService';
-
+import './App.css';
 
 
 const App = () => {
