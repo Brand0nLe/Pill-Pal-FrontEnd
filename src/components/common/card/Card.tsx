f9da76a4fcf8684e65b1ec61f36e95384d06b01a
Brand0nLe <93brandonle@gmail.com>
2023-05-22 08:33:57 -0700

    routed add med btn on card to schedules page

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import './Card.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Card.css';
 import defaultProfilePicture from '../../assets/images/default-profile-picture.png';
-
 import AddIcon from '@mui/icons-material/Add';
 
 interface CardProps {
-    name: string;
+  name: string;
 }
+
 export default function Card(props: CardProps) {
+  const navigate = useNavigate();
 
-    return (
-        <div className='card-parent'>
-            <div className="card-bg">
-                <div className="card-body">
-                    <img 
-                    id='' 
-                    src={defaultProfilePicture} 
-                    alt="" 
-                    
-                    />
-                    <p>{props.name}</p>
+  const handleAddMedicationClick = () => {
+    navigate('/schedulepage?openModal=true');
+  };
 
-                    <button>
-                        < AddIcon />
-                        Add Med
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="card-parent">
+      <div className="card-bg">
+        <div className="card-body">
+          <img id="" src={defaultProfilePicture} alt="" />
+          <p>{props.name}</p>
+
+          <button onClick={handleAddMedicationClick}>
+            <AddIcon />
+            Add Med
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
