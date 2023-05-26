c90079a7367d63f24980104bd1d3a849e676da70
Brand0nLe <93brandonle@gmail.com>
2023-05-26 14:51:55 -0700

    harrisons edits merge

--- src/components/pages/dependentspage/DependentsPage.tsx
+++ src/components/pages/dependentspage/DependentsPage.tsx
@@ -1,9 +1,11 @@
 import './DependentsPage.css'
 import NavBar from '../../navbarheader/NavBarHeader';
 import Card from '../../common/card/Card';
+import Footer from '../../footer/Footer';
 import React, { useState, useEffect } from 'react';
 import { Col, Container } from 'react-bootstrap';
 import AddNewBtn from '../../assets/images/add new dependent button.png'
+import AdminCard from '../../common/admin-card/AdminCard';
 
 export default function DependentsPage() {
   const [dependentName, setDependentName] = useState('');
@@ -29,8 +31,9 @@ export default function DependentsPage() {
       <NavBar />
       <Container className="dependents-divide">
         {/* Left side */}
-        <Col className="admin-container" md={4}>
-          <Card name='ADMIN' />
+        <Col className="admin-container" xs={12} md={4}>
+          {/* <Card name='ADMIN' /> */}
+          < AdminCard />
           <input
             type="text"
             value={dependentName}
@@ -41,12 +44,13 @@ export default function DependentsPage() {
           </button>
         </Col>
         {/* right side */}
-        <Col className="three-wide" md={8}>
+        <Col className="three-wide" xs={12} md={8}>
           {dependents.map((dependent, index) => (
             <Card key={index} name={dependent} />
           ))}
         </Col>
       </Container>
+      <Footer />
 
     </div>
   );
